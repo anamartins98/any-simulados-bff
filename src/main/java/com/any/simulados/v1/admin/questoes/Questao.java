97d0c05cf5d9aa68fe8da6330b9463b05fede6ee
Ana Martins <anacarolina.bnl@gmail.com>
2025-04-08 22:47:56 -0300

    Indicar atributos do Dynamo

--- src/main/java/com/any/simulados/v1/admin/questoes/Questao.java
+++ src/main/java/com/any/simulados/v1/admin/questoes/Questao.java
@@ -3,13 +3,13 @@ package com.any.simulados.v1.admin.questoes;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 
 import java.util.List;
 
 @DynamoDbBean
-@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Questao {
     private Long id;
@@ -33,5 +33,32 @@ public class Questao {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    @DynamoDbAttribute("enunciado")
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+
+    @DynamoDbAttribute("alternativas")
+    public List<String> getAlternativas() {
+        return alternativas;
+    }
+
+    public void setAlternativas(List<String> alternativas) {
+        this.alternativas = alternativas;
+    }
+
+    @DynamoDbAttribute("respostaCorreta")
+    public String getRespostaCorreta() {
+        return respostaCorreta;
+    }
+
+    public void setRespostaCorreta(String respostaCorreta) {
+        this.respostaCorreta = respostaCorreta;
     }
 }
